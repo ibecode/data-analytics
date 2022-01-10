06ee4093de49725931d3697f7cbe05e36c2ae099
Ibe Aboh <5abohi72@solent.ac.uk>
2022-01-10 01:30:49 +0100

    visual solution

--- visual.py
+++ visual.py
@@ -16,3 +16,25 @@ the data using Matplotlib.
 """
 
 # TODO: Your code here
+
+import database
+import matplotlib.pyplot as plt
+
+
+def top5_countries_confirmed():
+    ax = database.top_confirmed_cases()
+    x1, x2, x3, x4, x5 = ax[0][0], ax[1][0], ax[2][0], ax[3][0], ax[4][0]
+    y1, y2, y3, y4, y5 = ax[0][1], ax[1][1], ax[2][1], ax[3][1], ax[4][1]
+    # sizes = []
+    # labels = []
+    # for t in countries:
+    #     x, y, z = t
+    #     labels.append(x)
+    #     sizes.append(z)
+    labels = (x1, x2, x3, x4, x5)
+    values = [y1, y2, y3, y4, y5]
+    exp = [0, 0.1, 0, 0, 0]
+    plt.pie(values, labels=labels, autopct='%0.1f%%', explode=exp)
+    plt.title("Top 5 countries with confirmed cases")
+    plt.show()
+
